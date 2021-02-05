2f540552c4d2e50c92f4ab98420958c5fe604ee7
Suvansh Agrawal <s22ubbu@yahoo.com>
2021-02-05 08:54:51 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,15 @@
+interface Test{
+	void print();
+	}
+class A implements Test{
+	public void print(){
+		System.print.out("HELLO");
+		}
+	}
+
+class TestSubject{
+	pucblic static void main(String args[]){
+		A obj = new A();
+		A.print();
+		}
+	}
